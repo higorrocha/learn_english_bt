742880df5fb5c3969b48a4975de495d92a4f630b
Higor Rocha <you@example.com>
2021-09-11 10:39:43 -0300

    Create interface with GridView and MediaQuery

--- lib/screens/Animals.dart
+++ lib/screens/Animals.dart
@@ -10,6 +10,36 @@ class Animals extends StatefulWidget {
 class _AnimalsState extends State<Animals> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return GridView.count(
+        crossAxisCount: 2,
+        childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+        children: [
+          GestureDetector(
+            onTap: (){},
+            child: Image.asset("assets/images/cat.png"),
+          ),
+          GestureDetector(
+            onTap: (){},
+            child: Image.asset("assets/images/cow.png"),
+          ),
+          GestureDetector(
+            onTap: (){},
+            child: Image.asset("assets/images/dog.png"),
+          ),
+          GestureDetector(
+            onTap: (){},
+            child: Image.asset("assets/images/lion.png"),
+          ),
+          GestureDetector(
+            onTap: (){},
+            child: Image.asset("assets/images/monkey.png"),
+          ),
+          GestureDetector(
+            onTap: (){},
+            child: Image.asset("assets/images/sheep.png"),
+          ),
+        ],
+    );
   }
 }
